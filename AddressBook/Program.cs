16d58c752ca38dc7f3b7924c964450043ccb5716
ashwanikarma <ashwanikarma9@gmail.com>
2023-02-12 17:03:37 +0530

    [Ashwani] - Created a contact in address book

---
+++ AddressBook/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook Book System");
+
+            //add a new contact
+            CreateContact.PersonDetails();
+        }
+    }
+}
